8573b58331d108eecdcf4c31a571448efc70f7b5
조하민 <johamin@johamin-ui-MacBookAir.local>
2025-10-13 13:43:12 +0900

    ADD :: 이니셜 커밋

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+    ...nextConfig,
+    dest: 'public', // Service Worker가 public 폴더에 생성됨
+    register: true, // 자동 등록
+    skipWaiting: true, // 업데이트 즉시 활성화
+    disable: process.env.NODE_ENV === 'development', // 개발 단계에선 비활성화
+});
